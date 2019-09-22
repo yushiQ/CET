e1dc9fabbf7a23053e352cfd5f99aee6989199d5
yushiQ <shiquan7139@163.com>
2019-09-22 20:12:19 +0800

    接口开发

--- dva-project/src/page/headmaster/index.js
+++ dva-project/src/page/headmaster/index.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect } from "react";
+import React, { useState,useEffect } from "react";
 // import { connect } from "dva";
 import styles from "./index.css";
 // import {login,sign} from "../../services/example"
@@ -10,9 +10,14 @@ import Listchart from "../../components/lineChart";
 import Histogram from "../../components/histogram";
 
 function Headermaster() {
+  let [button,setButton]=useState(true)
   //初始数据
-  useEffect(() => {}, []);
+  useEffect(() => { }, []);
 
+let setDataStyle=()=>{
+  setButton(!button)
+}
+console.log(button)
   return (
     <div className={styles.masterpage}>
       <header className={styles.header}>重点关注学生考试成绩统计表</header>
@@ -41,10 +46,11 @@ function Headermaster() {
           <div className={styles.item}>
             <span>张三,张三,张三</span>
             <span>(3/人)</span>
+            <span className={styles.Btns}><i className={styles.btnBox} onClick={setDataStyle}><b className={styles.icon}></b></i>柱状图/线圈</span>
           </div>
         </div>
       </div>
-      <Listchart />
+      <Listchart button={button}/>
       <Histogram />
     </div>
   );
